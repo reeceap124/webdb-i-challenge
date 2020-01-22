4674bb9233f6e6dcb30aeac699df999a96d675ef
Reece Pierson <piersonreece@gmail.com>
2020-01-22 10:32:46 -0600

    made MVP

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
-
 const server = express();
+const router = require('./router')
 
 server.use(express.json());
+
+server.use('/api', router)
 
 module.exports = server;
